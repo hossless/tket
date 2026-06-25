1b77ea425672cadceecaa31f6a94f6e247ce6a58
HO <hosseinlesani77@gmail.com>
2026-06-25 22:55:30 +0330

    feat: add tickets table

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -25,3 +25,18 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT chk_contact_method CHECK (email IS NOT NULL OR phone_number IS NOT NULL)
 );
+
+CREATE TABLE tickets (
+    ticket_id SERIAL PRIMARY KEY,
+    sport_type VARCHAR(20) NOT NULL,
+    home_team VARCHAR(50) NOT NULL,
+    away_team VARCHAR(50) NOT NULL,
+    ticket_date_time TIMESTAMP NOT NULL,
+    venue_city VARCHAR(50) NOT NULL,
+    price NUMERIC(12, 2) NOT NULL,
+    total_capacity INT NOT NULL,
+    remaining_capacity INT NOT NULL,
+    category VARCHAR(20) DEFAULT 'Normal' NOT NULL,
+    CONSTRAINT chk_positive_numbers CHECK (total_capacity > 0 AND remaining_capacity >= 0 AND price >= 0),
+    CONSTRAINT chk_logical_capacity CHECK (remaining_capacity <= total_capacity)
+);

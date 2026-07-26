81e546b4aad87f21363203994b48b33e94eb723d
HO <hosseinlesani77@gmail.com>
2026-07-27 00:07:29 +0330

    feat: save api 4 results in redis

--- core/views/get_cities_and_venues_list.py
+++ core/views/get_cities_and_venues_list.py
@@ -1,5 +1,9 @@
+import json
+import redis
 from django.db import connection
 from django.http import JsonResponse
+
+cache = redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
 
 # API 4: Get Cities and Venues List
     # Retrieves a distinct list of all available venues and their corresponding cities 
@@ -8,11 +12,19 @@ def get_cities_and_venues_list(request):
     if request.method != 'GET':
         return JsonResponse({"error": "Method not allowed. Use GET."}, status=405)
 
+    cached_venues = cache.get("cities_and_venues_list")
+    if cached_venues:
+        return JsonResponse({
+            "source": "Redis", 
+            "venues": json.loads(cached_venues)
+        }, status=200)
+
     with connection.cursor() as cursor:
         cursor.execute("""
             SELECT DISTINCT md.venue_name, t.venue_city 
             FROM tickets t
-            JOIN match_details md ON t.ticket_id = md.ticket_id;
+            JOIN match_details md ON t.ticket_id = md.ticket_id
+            WHERE md.venue_name IS NOT NULL AND t.venue_city IS NOT NULL;
         """)
         raw_data = cursor.fetchall()
 
@@ -23,4 +35,9 @@ def get_cities_and_venues_list(request):
             "venue_city": row[1]
         })
 
-    return JsonResponse({"venues": formatted_response}, status=200)
+    cache.set("cities_and_venues_list", json.dumps(formatted_response), ex=86400)
+
+    return JsonResponse({
+        "source": "Database", 
+        "venues": formatted_response
+    }, status=200)

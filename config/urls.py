ce6d1cc2a8f968d147f281e725e48d38e342423d
HO <hosseinlesani77@gmail.com>
2026-07-27 17:25:54 +0330

    fix: update urls to match restful design

--- config/urls.py
+++ config/urls.py
@@ -20,18 +20,23 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('api/auth/login/', views.login),
     path('api/auth/signup/request/', views.request_signup_otp),
     path('api/auth/signup/verify/', views.verify_signup_otp),
-    path('api/users/update/', views.update_user_profile),
+    
+    path('api/user/profile/', views.update_user_profile),
+    path('api/user/reservations/', views.get_user_bookings),
+    path('api/user/reservations/cancel/', views.cancel_ticket_and_refund),
+    path('api/user/reports/', views.report_ticket_issue),
+    
     path('api/tickets/cities-venues/', views.get_cities_and_venues_list),
     path('api/tickets/search/', views.search_tickets),
     path('api/tickets/<int:ticket_id>/', views.get_ticket_details),
+    
     path('api/tickets/reserve/', views.reserve_ticket),
     path('api/tickets/pay/', views.payment_for_ticket),
     path('api/tickets/reservations/<int:reservation_id>/penalty/', views.check_cancellation_penalty),
+    
     path('api/admin/manage/', views.admin_ticket_management),
-    path('api/user/reservations/', views.get_user_bookings),
-    path('api/user/reservations/cancel/', views.cancel_ticket_and_refund),
-    path('api/user/reports/submit/', views.report_ticket_issue),
 ]
